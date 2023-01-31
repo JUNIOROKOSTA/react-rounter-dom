8228bbe80add1f4f77dac060ccd490f3f1a1c30f
JUNIOR_OKOSTA <82908127+JUNIOROKOSTA@users.noreply.github.com>
2023-01-31 15:39:21 -0300

    init Route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { HomeReact } from './templates/homeReact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Base de configurações Padrão Okosta para desenvolvimento em React.</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <HomeReact />
     </div>
   );
 }
---
+++ src/templates/homeReact/index.jsx
@@ -0,0 +1,13 @@
+import logo from '../../logo.svg';
+
+export const HomeReact = () => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Base de configurações Padrão Okosta para desenvolvimento em React.</p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+    </header>
+  );
+};
